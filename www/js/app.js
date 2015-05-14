66a5af33be8878632a59d7f0ec9650b29dcb7e79
luohao <553569682@qq.com>
2015-05-14 14:40:05 +0800

    first

--- www/js/app.js
+++ www/js/app.js
@@ -57,22 +57,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
 
-  .state('tab.account', {
-    url: '/account',
+  .state('tab.settings', {
+    url: '/settings',
     views: {
       'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+        templateUrl: 'templates/tab-settings.html',
+        controller: 'SettingsCtrl'
       }
     }
   });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,32 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+  $scope.data = {};
+  $scope.data.slides = [
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+  ];
+
+  $scope.generateData = function(){
+    $scope.data.slides = [];
+    for(var i=1; i<=5; i++){
+      var item = {
+        title : "Slide " + i,
+        data  : "http://d.hiphotos.baidu.com/zhidao/pic/item/562c11dfa9ec8a13e028c4c0f603918fa0ecc0e4.jpg"
+      }
+      $scope.data.slides.push(item);
+    }
   }
+
+  $scope.$on("$ionicView.loaded", function(){
+    $scope.generateData();
+  });
+
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatsCtrl', function($scope) {
+
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('SettingsCtrl', function($scope) {
+
 });
